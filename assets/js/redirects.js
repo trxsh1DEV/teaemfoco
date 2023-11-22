aed52a6a9da29197f13253c60adcea8bc8ba35e9
trxsh1DEV <yagosousa630@gmail.com>
2023-11-22 20:42:47 -0300

    fixed the size of texts of the pages  when contain just texts and are open in new tabs

--- assets/js/redirects.js
+++ assets/js/redirects.js
@@ -8,5 +8,6 @@ const redirectElements = (id) => {
 
 const redirectPage = (page) => {
   // const myBtn = document.getElementById(id);
-  window.location.href = `./assets/pages/carousel/${page}.html`;
+  // window.location.href = `./assets/pages/carousel/${page}.html`;
+  window.open(`./assets/pages/carousel/${page}.html`, "_blank");
 };
